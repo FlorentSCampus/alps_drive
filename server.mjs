70bdd71692bae27158fa0a13dd51c66baadec155
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-04-12 14:06:34 +0200

    FEAT: modify code for get files size

--- server.mjs
+++ server.mjs
@@ -1,21 +1,24 @@
 export default class Server {
-    constructor(express, fs, os, path, port) {
+    constructor(express, fs, os, path, process, port) {
+        this.dirName = "/alps_drive"
+
         this.app = express
         this.fs = fs
         this.os = os
         this.path = path
         this.port = port
+        this.process = process
 
         this.tmp = os.tmpdir()
-        this.tmpDir = "/alps_drive"
+        this.dirPath = this.tmp + this.dirName
     }
 
     start = () => {
         this.app.use((req, res, next) => {
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.setHeader("Access-Control-Allow-Methods", "*");
-            res.setHeader("Access-Control-Allow-Headers", "*");
-            next();
+            res.setHeader("Access-Control-Allow-Origin", "*")
+            res.setHeader("Access-Control-Allow-Methods", "*")
+            res.setHeader("Access-Control-Allow-Headers", "*")
+            next()
         })
         
         this.app.get("/", (req, res) => {
@@ -25,60 +28,58 @@ export default class Server {
         this.app.listen(this.port, () => {
             console.log(`Example app listening on port ${this.port}`)
         })
+
+        this.createTmpFolder()
+        this.getDirs()
+    }
+
+    getDirs = (dirPath = this.dirPath) => {
+        this.app.get("/api/drive", async (req, res, next) => {
+            try {
+                const files = await this.fs.promises.readdir(dirPath, { withFileTypes: true })
+                const filesFormat = files.map(file => {
+                    return {
+                        name: file.name,
+                        size: this.fs.statSync(this.path.join(dirPath, file.name)).size,
+                        isFolder: file.isDirectory()
+                    }
+                })
+
+                return res.status(200).send(filesFormat)
+            } catch (err) {
+                return res.status(500).send(`Cannot get directories: ${err}`)
+            }
+        })
     }
 
     getName = (req) => {
-        if (req.query.name !== undefined) {
-            return req.query.name
-        } else {
-            return req.params.name
+        return req.query.name !== undefined ? req.query.name : req.params.name
+    }
+
+    createTmpFolder = () => {
+        if (!this.fs.existsSync(this.dirPath)) {
+            this.fs.promises.mkdir(this.dirPath, { recursive: true })
         }
     }
 
     createDir = () => {
-        const tmpDirPath = this.tmp + this.tmpDir
-
-        if (this.fs.existsSync(tmpDirPath)) {
-            this.fs.promises.mkdir(tmpDirPath, { recursive: true })
-        }
-
         this.app.post("/api/drive", async (req, res, next) => {
             const name = this.getName(req)
-            const dirPath = this.path.join(tmpDirPath, "/", name)
+            const dirPath = this.path.join(this.dirPath, "/", name)
 
             try {
                 await this.fs.promises.mkdir(dirPath, { recursive: true })
                 return res.sendStatus(201)
             } catch (err) {
-                return res.status(500).send(`Cannot create the directory: ${err}`)
+                return res.status(500).send(`Cannot create (POST) the directory: ${err}`)
             }
         })
     }
 
-    getDirs = () => {
-        this.createDir()
-
-        this.app.get("/api/drive", async (req, res, next) => {
-            const files = await this.fs.promises.readdir(this.tmp + this.tmpDir, { withFileTypes: true })
-            const filesFormat = files.map(file => {
-                const obj = {
-                    name: file.name,
-                    isFolder: file.isDirectory()
-                }
-
-                return obj
-            })
-
-            res.send(filesFormat)
-        })
-    }
-
     deleteDir = () => {
-        const tmpDirPath = this.tmp + this.tmpDir
-
         this.app.delete("/api/drive/:name", (req, res, next) => {
             const name = this.getName(req)
-            const dirPath = this.path.join(tmpDirPath, "/", name)
+            const dirPath = this.path.join(this.dirPath, "/", name)
 
             this.fs.rm(dirPath, { recursive: true }, (err) => {
                 if (err) {
@@ -87,6 +88,31 @@ export default class Server {
                     return res.sendStatus(200)
                 }
             })
+        })
+    }
+
+    /**
+     * TO DO LATER
+     */
+    changeDir = () => {
+        this.app.get("/api/drive/:name", async (req, res, next) => {
+            try {
+                const name = this.getName(req)
+                const dirPath = this.path.resolve(this.dirPath + "/" + name)
+                console.log(dirPath);
+                // const dirPath = this.dirPath + "/" + name
+                const files = await this.fs.promises.readdir(dirPath, { withFileTypes: true })
+                const filesFormat = files.map(file => {
+                    return {
+                        name: file.name,
+                        isFolder: file.isDirectory()
+                    }
+                })
+
+                return res.status(200).send(filesFormat)
+            } catch (err) {
+                return res.status(500).send(`Cannot changed directory: ${err}`)
+            }
         })
     }
 }
